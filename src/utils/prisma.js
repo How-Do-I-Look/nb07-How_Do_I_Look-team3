b191b86542fc5d67ffaf07c1b2abd5068207977c
gy <podojjang_kr@naver.com>
2025-12-08 18:29:31 +0900

    feat:프로젝트 초기 설정 및 오류 수정
    * feat: 프로젝트 초기 작성
    
    * feat: 프로젝트 환경설정 수정
    
    * feat:프로젝트 초기설정
    
    * hotpix:경로오류수정
    
    * feat:초기설정 중 충돌오류 수정
    
    * feat: 재요청

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -7,15 +7,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const require = createRequire(import.meta.url);
-dotenv.config({ path: join(__dirname, "../.env") });
-const { PrismaClient } = require("../generated/prisma/index.js");
-//import { PrismaClient } from "@prisma/client";
+
+dotenv.config({ path: join(__dirname, "../../.env") });
+const { PrismaClient } = require("../../generated/prisma/index.js");
+
+
 
 import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "pg";
 
 const connectionString = process.env.DATABASE_URL;
-console.log("Prisma connection string:", connectionString);
+
+
 const pool = new pkg.Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({
