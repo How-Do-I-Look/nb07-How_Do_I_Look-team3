9b472c21f7d00544ef036c930a9fbf9ab72151f1
gy <podojjang_kr@naver.com>
2025-12-08 07:36:46 +0000

    feat: 프로젝트 환경설정 수정

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -7,8 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const require = createRequire(import.meta.url);
-dotenv.config({ path: join(__dirname, "../.env") });
-const { PrismaClient } = require("../generated/prisma/index.js");
+dotenv.config({ path: join(__dirname, "../../.env") });
+const { PrismaClient } = require("../../generated/prisma/index.js");
 //import { PrismaClient } from "@prisma/client";
 
 import { PrismaPg } from "@prisma/adapter-pg";
