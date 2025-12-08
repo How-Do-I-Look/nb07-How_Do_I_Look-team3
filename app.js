bc7e4cdaa40d5afccfc3ed2684bcfabccddee502
gy <podojjang_kr@naver.com>
2025-12-08 08:32:58 +0000

    feat:초기설정 중 충돌오류 수정

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 const app = express();
+// ⭐️ 이 부분을 남깁니다. (충돌 해소)
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 BigInt.prototype.toJSON = function () {
   return this.toString();
 };
 
 app.listen(PORT, () => {
-  console.log("서버 실행 중");
+  console.log(`서버 실행 중 (포트: ${PORT})`);
 });
