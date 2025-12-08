3d661caeb79f69d98d4b37b0812573a63d67a98a
gy <podojjang_kr@naver.com>
2025-12-08 07:55:06 +0000

    feat:프로젝트 초기설정

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import "./src/utils/dotenv.js";
 import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 BigInt.prototype.toJSON = function () {
   return this.toString();
