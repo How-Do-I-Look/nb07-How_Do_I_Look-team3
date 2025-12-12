38b7908508398f691e87546d73b43eea314c94cf
Bora Lee <dltngus1317@gmail.com>
2025-12-12 18:33:38 +0900

    feat: 스타일 조회 경로,라우터명 수정

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,14 @@ import { errorHandler } from "./src/errors/errorHandler.js";
 import curationRouter from "./src/routes/curation/curation.route.js";
 import styleRouter from "./src/routes/style/style.route.js";
 import stlyeReadRouter from "./src/routes/curation/styleRead.route.js";
+import commentRouter from "./src/routes/comment/comment.route.js";
+import rankRouter from "./src/routes/style/ranking.route.js";
 import { multerUtil as multer } from "./src/utils/multer.js";
 import { createStyleImage } from "./src/services/style/style.service.js";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 
 
@@ -35,6 +36,8 @@ BigInt.prototype.toJSON = function () {
 app.use("/curations", curationRouter);
 app.use("/styles", styleRouter);
 app.use("/style", stlyeReadRouter);
+app.use("/ranking", rankRouter);
+app.use("/comments", commentRouter);
 app.post("/images", multer.single("image"), (req, res, next) => {
   try {
     const uploadFile = req.file;
@@ -45,6 +48,10 @@ app.post("/images", multer.single("image"), (req, res, next) => {
     next(error);
   }
 });
+
+
+
+
 
 
 app.use(errorHandler);
