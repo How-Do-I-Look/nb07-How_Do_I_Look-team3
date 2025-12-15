947ec9ecfbe17fd649fbc830c7534689b31fbd70
Bora Lee <dltngus1317@gmail.com>
2025-12-15 18:34:21 +0900

    feat:갤러리 목록 조회 구현

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ import tagRouter from "./src/routes/tag/tag.route.js";
 import styleRouter from "./src/routes/style/style.route.js";
 import styleReadRouter from "./src/routes/style/styleRead.route.js";
 import commentRouter from "./src/routes/comment/comment.route.js";
-import rankRouter from "./src/routes/style/ranking.route.js";
 import { multerUtil as multer } from "./src/utils/multer.js";
 import { createStyleImage } from "./src/services/style/style.service.js";
 import path from "path";
@@ -41,7 +40,6 @@ app.use("/tags", tagRouter);
 app.use("/curations", curationRouter);
 app.use("/styles", styleRouter);
 app.use("/style", styleReadRouter);
-app.use("/ranking", rankRouter);
 app.use("/comments", commentRouter);
 app.post("/images", multer.single("image"), (req, res, next) => {
   try {
