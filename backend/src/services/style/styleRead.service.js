d174489f86a31c1a49ec959f2d6fa85c56d3afc5
Bora Lee <dltngus1317@gmail.com>
2025-12-16 15:45:52 +0900

    fix:페이지네이션 구현 후 검색 오류 수정

--- backend/src/services/style/styleRead.service.js
+++ backend/src/services/style/styleRead.service.js
@@ -1,6 +1,6 @@
 
 import { Stylelist } from "../../classes/style/styleread.js";
-import { getPagination } from "../../utils/style/pagenation.utile.js";
+import { calculateTotalPages, getPaginationMetadata } from "../../utils/style/pagenation.utile.js";
 import { stylesRepository } from "./stylerepository.js";
 
 
@@ -24,31 +24,37 @@ export async function getGalleryStyles({
         searchField = 'tags'
       }
 
-  // Repository 호출 (totalItemCount: 전체개수 ) 조회
-      const totalItemCount = await stylesRepository.countStyles({
-      searchBy: searchField,
-      keyword: keyword,
-      tag: tag,
-      });
+      // WHERE 조건 생성 (Repository 위임)
+    const whereConditions = stylesRepository.getPrismaWhereClause({
+        searchBy: searchField,
+        keyword: keyword,
+        tag: tag,
+        });
 
-  //페이지네이션 메타데이터 계산
-  const { currentPage, totalPages, skip } = getPagination({
-    totalItemCount,
-    page,
-    pageSize
-});
+        //전체 개수 조회
+    const totalItemCount = await stylesRepository.countStyles(whereConditions);
 
-    //Repository 호출  계산된 skip 값을 사용하여 실제 데이터를 조회
-  const styles = await stylesRepository.findStylesWithOffset({
-    skip, // (offset) 값
-    take: pageSize, //(Limit)
-    sortBy,
-    searchBy: searchField,
-    keyword: keyword,
-    tag: tag,
-  });
+     //페이지네이션 메타데이터 계산
+    const { currentPage, totalPages, skip } = getPaginationMetadata({
+        totalItemCount,
+        page,
+        pageSize
+    });
+
+    //실제 데이터 조회
+    const styles = await stylesRepository.findStylesWithOffset({
+        skip,
+        take: pageSize,
+        sortBy,
+        where: whereConditions,
+    });
+
   const data = styles.map(style =>
     Stylelist.fromPrismaEntity(style)
   )
 return { currentPage, totalPages, totalItemCount, data };
 }
+
+
+// - 등록된 스타일 랭킹 목록을 조회할 수 있습니다.
+// - `rankBy` : total | trendy | personality | practicality, costEffectiveness (랭킹 기준)
