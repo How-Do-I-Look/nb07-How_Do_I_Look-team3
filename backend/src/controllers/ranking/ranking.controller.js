3c6b669f984a3f1b1cf4b63ac43b14f1fa2d948b
gy <podojjang_kr@naver.com>
2025-12-22 08:47:48 +0000

    feat: 랭킹 컨트롤러 추가

---
+++ backend/src/controllers/ranking/ranking.controller.js
@@ -0,0 +1,21 @@
+import * as styleService from "../../services/style/style.service";
+import {
+  validateLimit,
+  validateRankBy,
+} from "../../classes/ranking/ranking.js";
+class RankingController {
+  async listStyleRanking(req, res) {
+    const { rankBy = "total", cursor, limit = 10 } = req.query;
+    validateRankBy(rankBy);
+    validateLimit(limit);
+
+    const rankingStyles = await styleService.listStyleRanking(
+      rankBy,
+      cursor,
+      limit,
+    );
+    res.status(200).json(rankingStyles);
+  }
+}
+
+export const rankingController = new RankingController();
