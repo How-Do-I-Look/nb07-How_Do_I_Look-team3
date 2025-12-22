b26f9b39218f27884c0860a5b9f72150a6cc08df
Henry <101556773+HenryVoid@users.noreply.github.com>
2025-12-22 16:13:27 +0900

    fix: '전체' 태그 추가 로직 제거
    frontend 에서 자동 '전체' 태그 추가중

--- backend/src/services/tag/tag.service.js
+++ backend/src/services/tag/tag.service.js
@@ -13,7 +13,6 @@ export const getTags = async () => {
   });
 
   const tagObjects = tags.map((tag) => new Tag(tag).name);
-  tagObjects.unshift("전체"); // 전체 태그를 맨 앞에 추가
 
   return { tags: tagObjects };
 };
